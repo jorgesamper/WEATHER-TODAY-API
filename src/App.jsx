f6e7b006f10dd05c7cdcc2faa0a1e0b3b84f00c5
jorgesamper <jorgesamper7@gmail.com>
2023-12-07 18:23:35 +0100

    components-app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import '@fortawesome/fontawesome-free/css/all.css';
 import Navbar from './components/Navbar';
-import WeatherIcon from './components/WeatherIcon'; // Asegúrate de ajustar la ruta según la ubicación real
+import WeatherIcon from './components/WeatherIcon';
+
+import Footer from './components/Footer';
+
 
 import './App.css';
 
@@ -10,51 +13,9 @@ function App() {
 
   return (
     <>
-          <WeatherIcon />
-
-      <div></div>
+      <WeatherIcon />
       <h1>WEATHER TODAY</h1>
-      
-
-      <footer className="bg-body-light text-white">
-        <div className="container pt-5 pb-2">
-          <p className="text-secondary text center">
-            Powered by:{' '}
-            <a
-              href="https://www.weatherapi.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="Weather API"
-            >
-              WeatherAPI.com
-            </a>
-          </p>
-          <p className="text-secondary text-center">Made by Jorge Samper</p>
-
-          {/* ICONS */}
-          <div className="text-center text-secondary">
-            <a
-              href="https://www.linkedin.com/in/jorgesamperarias/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-secondary me-3"
-            >
-              <i className="fab fa-linkedin fa-2x"></i>
-            </a>
-            <a
-              href="https://github.com/jorgesamper"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-secondary"
-            >
-              <i
-                className="fab fa-github fa-2x"
-                style={{ marginLeft: '15px' }}
-              ></i>
-            </a>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,44 @@
 const Footer = () => {
     return (
         <footer className="bg-body-light text-white">
-            <div className="container pt-5 pb-2">
-                <p className="text-secondary text center">
-                    Powered by:{" "}
-                    <a href="https://www.weatherapi.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title="Weather API"
-                    >
-                        WeatherAPI.com
-                    </a>
-                </p>
-                <p class="text-secondary text-center">
-                    Weather today made by Jorge Samper
-                </p>
-                <div className="text-center text-secondary">
-                    <a
-                        href="https://www.linkedin.com/in/jorgesamperarias/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-secondary me-3"
-                    >
-                        <i className="fab fa-linkedin fa-2x"></i>
-                    </a>
-                    <a
-                        href="https://github.com/jorgesamper"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-secondary"
-                    >
-                        <i className="fab fa-github fa-2x"></i>
-                    </a>
-                </div>
-            </div>
-        </footer>
+        <div className="container pt-5 pb-2">
+          <p className="text-secondary text center">
+            Powered by:{' '}
+            <a
+              href="https://www.weatherapi.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Weather API"
+            >
+              WeatherAPI.com
+            </a>
+          </p>
+          <p className="text-secondary text-center">Made by Jorge Samper</p>
+
+          {/* ICONS */}
+          <div className="text-center text-secondary">
+            <a
+              href="https://www.linkedin.com/in/jorgesamperarias/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-secondary me-3"
+            >
+              <i className="fab fa-linkedin fa-2x"></i>
+            </a>
+            <a
+              href="https://github.com/jorgesamper"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-secondary"
+            >
+              <i
+                className="fab fa-github fa-2x"
+                style={{ marginLeft: '15px' }}
+              ></i>
+            </a>
+          </div>
+        </div>
+      </footer>
     );
 };
 
